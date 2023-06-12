9b2c42c670fdecb303e920966c8846cae7b51b44
rabbitrose <2908415008@qq.com>
2023-06-12 23:42:57 +0800

    Submit at 2023/6/12

--- MemoryManagement.py
+++ MemoryManagement.py
@@ -4,35 +4,35 @@ import re
 class MemoryManager:
     def __init__(self, allocator):
         self.allocator = allocator
-    
     def allocate(self, process, request_size):
         memory_view = self.allocator.memory_view()
-
-        # You need to complete the code here. 
-
-        # The input of this function contains two parameters: 
-        #   process -- the process requesting memory, you don't need to
-        #              do anything for process, just pass it to the 
-        #              'self.allocator.allocate_memory' function at the 
-        #              end of this function.
-        #   request_size -- an integer indicating how many memory blocks 
-        #                   this process requests.
-
-        # The first line returns 'memory_view', a list of memory blocks.
-        # If a memory block is free, the corresponding item of the list
-        # will be None, otherwise the item will be the process object.
-        
-        # The total size of the memory is 256 blocks.
-
-        # You need to decide which memory to allocate to the process based
-        # on 'memory_view' and 'request_size'. When you make a decision, 
-        # pass the starting address of the memory (i.e. 'block_start')
-        # along with 'request_size' and 'process' to the function
-        # 'self.allocator.allocate_memory' (see below).
-
-        # Memory blocks will be automatically reclaimed according to 
-        # the definition in the process objects.
-
-        self.allocator.allocate_memory(block_start, request_size, process)
+        # Find consecutive free blocks in the memory block list to meet the requested memory size here is "first fit"
+        start = -1
+        count = 0
+        for i, block in enumerate(memory_view):
+            if block is None:
+                count += 1
+                if count == request_size:#Enough space for put a process
+                    start = i - request_size + 1
+                    break
+            else:
+                count = 0
+        if start != -1:# If a suitable block of memory is found, it is allocated to the process
+            self.allocator.allocate_memory(start, request_size, process)
+        else:#Could not find a suitable contiguous block of memory
+            # Attempt to free the memory block and reallocate
+            self.release_and_allocate()
+            self.allocate(process, request_size)
+    def release_and_allocate(self):
+        memory_view = self.allocator.memory_view()
+        # free memory block and reallocate
+        for i, block in enumerate(memory_view):
+            if block is not None:#If there is a process in this place
+                #free the memory block
+                self.allocator.free_memory(block)
+                # Attempt to reallocate memory block
+                request_size = block.get_memory()
+                #print(request_size)
+                self.allocate(block, request_size[1])
 
 
